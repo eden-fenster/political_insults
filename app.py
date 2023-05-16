ccca832c5dcd4f51de797a593aa55845c212511f
eden-fenster <eden.fenster@gmail.com>
2023-05-16 09:36:08 +0300

    * It's not getting info from the form for some reason.

--- app.py
+++ app.py
@@ -35,16 +35,19 @@ def fight():
     skin_color_2 = request.form.get('skincolor2')
     political_ideology_2 = request.form.get('politicalideology2')
     logging.debug(f"Input is {gender_2} \n {pronouns_2} \n {age_2} \n {skin_color_2} \n {political_ideology_2}")
+    return "Hello World"
     # Put into dict.
-    dict_to_send: dict = {'Gender': gender, 'Pronouns': pronouns, 'Age': age, 'Skin Color': skin_color,
-                          'Political Ideology': political_ideology}
-    # Sent to processor.
-    requests.post('http://127.0.0.1:8000', json=dict_to_send)
-    logging.debug("Sent")
-    # Fight !
-    response = requests.get('http://127.0.0.1:8000')
-    logging.debug("Got Response")
-    return response.json()
+    # dict_to_send: dict = {'Gender 1': gender_1, 'Pronouns 1': pronouns_1, 'Age 1': age_1, 'Skin Color 1': skin_color_1,
+    #                       'Political Ideology 1': political_ideology_1, 'Gender 2': gender_2,
+    #                       'Pronouns 2': pronouns_2, 'Age 2': age_2, 'Skin Color 2': skin_color_2,
+    #                       'Political Ideology 2': political_ideology_2}
+    # # Sent to processor.
+    # requests.post('http://127.0.0.1:8000', json=dict_to_send)
+    # logging.debug("Sent")
+    # # Fight !
+    # response = requests.get('http://127.0.0.1:8000')
+    # logging.debug("Got Response")
+    # return response.json()
 
 
 if __name__ == "__main__":
--- processor.py
+++ processor.py
@@ -1,6 +1,6 @@
 import json
-
-from flask import Flask, request, logging
+import logging
+from flask import Flask, request
 
 from main import fight
 from person import Person
@@ -30,21 +30,22 @@ def get_fight():
 @app.route('/people', methods=['POST'])
 def add_grids():
     # Add person to list.
+    logging.debug("Received")
     people.append(request.get_json())
-    if len(people) == 2:
-        # Put the people into Person objects, calling make_person.
-        p1: Person = Person(gender=people[0]['Gender'], pronouns=people[0]['Pronouns'],
-                            age=people[0]['Age'], skin_color=people[0]['Skin Color'],
-                            political_ideology=people[0]['Political Ideology'])
-        p2: Person = Person(gender=people[1]['Gender'], pronouns=people[1]['Pronouns'],
-                            age=people[1]['Age'], skin_color=people[1]['Skin Color'],
-                            political_ideology=people[1]['Political Ideology'])
-        # Fight
-        fight_string: str = fight(one_who_is_making_statement=p1, one_who_might_get_triggered=p2)
-        logging.debug(f"fight is {fight_string}")
-        # Put fight log inside string and return that.
-        fight_log.append(fight_string)
-        # Need to learn how to print in real time on webpage.
+
+    # Put the people into Person objects, calling make_person.
+    p1: Person = Person(gender=people['Gender 1'], pronouns=people['Pronouns 1'],
+                        age=people['Age 1'], skin_color=people['Skin Color 1'],
+                        political_ideology=people['Political Ideology 1'])
+    p2: Person = Person(gender=people['Gender 2'], pronouns=people['Pronouns 2'],
+                        age=people['Age 2'], skin_color=people['Skin Color 2'],
+                        political_ideology=people['Political Ideology 2'])
+    # Fight
+    fight_string: str = fight(one_who_is_making_statement=p1, one_who_might_get_triggered=p2)
+    logging.debug(f"fight is {fight_string}")
+    # Put fight log inside string and return that.
+    fight_log.append(fight_string)
+    # Need to learn how to print in real time on webpage.
     return '', 204
 
 
